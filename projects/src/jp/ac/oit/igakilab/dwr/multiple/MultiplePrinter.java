51a2d62c1f3abdcdc779d84054290b209a5140ea
Koike Takaaki <taka.vao.321@gmail.com>
2016-06-15 15:56:09 +0900

    helloworldが動作

---
+++ projects/src/jp/ac/oit/igakilab/dwr/multiple/MultiplePrinter.java
@@ -0,0 +1,14 @@
+package jp.ac.oit.igakilab.dwr.multiple;
+
+public class MultiplePrinter {
+    /**
+     * REST呼び出しを行う場合は下記のように呼ぶ
+     * http://sample.com:8080/project_name/dwr/jsonp/ClassName/MethodName/param1/ と指定する
+     * http://localhost:8080/multiple-dwr/dwr/jsonp/MultiplePrinter/helloWorld/ryokun/
+     * @param name
+     * @return nameに":HelloWorld"を付与したものを返す
+     */
+    public String helloWorld(String name){
+    	return name + ":HelloWorld";
+    }
+}
